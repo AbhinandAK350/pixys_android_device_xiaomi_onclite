6086fb1b831076fca257c8fef4a97e898b4cf7ee
AbhinandAK350 <abhinandak@outlook.com>
2020-11-14 06:46:05 +0100

    onclite: Update fp to redfin november

--- stag_onclite.mk
+++ stag_onclite.mk
@@ -22,9 +22,9 @@ PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
 PRODUCT_MANUFACTURER := Xiaomi
 
-BUILD_FINGERPRINT := "xiaomi/onc/onc:9/PKQ1.181021.001/V11.0.3.0.PFLMIXM:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RD1A.201105.003/6886239:user/release-keys"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="onc-user 9 PKQ1.181021.001 V11.0.3.0.PFLMIXM release-keys"
+    PRIVATE_BUILD_DESC="redfin-user 11 RD1A.201105.003 6886239 release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
